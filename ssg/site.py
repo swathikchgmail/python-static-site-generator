53b73a2257be7a783002b5f0b3695f25308cab2f
swathi.chadalavada <swathi.chadalavada@servicenow.com>
2022-03-31 17:41:33 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -6,6 +6,7 @@ class Site():
         self.source = Path(source)
         self.dest = Path(dest)
 
+
 def create_dir(self, path):
     directory = self.dest / path.relative_to(self.source)
     directory.mkdir(parents=True, exist_ok=True)
