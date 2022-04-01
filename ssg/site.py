2ac3fa04ad745a599cc1d514bd7c294b8367b22f
swathi.chadalavada <swathi.chadalavada@servicenow.com>
2022-03-31 17:43:10 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -6,14 +6,12 @@ class Site():
         self.source = Path(source)
         self.dest = Path(dest)
 
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
 
-def create_dir(self, path):
-    directory = self.dest / path.relative_to(self.source)
-    directory.mkdir(parents=True, exist_ok=True)
-
-
-def build(self):
-    self.dest.mkdir(parents=True, exist_ok=True)
-    for path in self.source.rglob("*"):
-        if path.is_dir():
-            self.create_dir(path)
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
