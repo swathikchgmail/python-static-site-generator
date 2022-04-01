069ddf24361527522ddb92ab20fb854383cb147d
swathi.chadalavada <swathi.chadalavada@servicenow.com>
2022-03-31 17:55:50 -0700

    Update parsers.py

---
+++ ssg/parsers.py
@@ -0,0 +1,20 @@
+from typing import List
+from pathlib import Path
+
+
+class Parser():
+    List[str]
+    extensions = []
+
+    def valid_extension(self, extension):
+        pass
+
+    def parse(self, path, source, dest):
+        pass
+
+    def read(self, path):
+        pass
+
+    def write(self, ):
+        full_path = self.dest
+        pass
